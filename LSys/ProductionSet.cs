53be13218bf1173540b22da1d8b60401c4a238a2
John Jepson <JohnMJep@googlemail.com>
2016-08-09 12:22:00 +0100

    Update ProductionSet.cs
    - Removed unnecessary references
    - Added comments

--- LSys/ProductionSet.cs
+++ LSys/ProductionSet.cs
@@ -1,31 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LSys
 {
+    /// <summary>
+    /// Class to handle L-System production sets
+    /// </summary>
     public class ProductionSet
     {
+        #region Fields
         public List<List<Production>> _productionSet = new List<List<Production>>();
-
         private Random rand = new Random();
+        #endregion Fields
 
-        public ProductionSet()
-        {
-        }
+        #region Constructors
+        public ProductionSet() { }
+        #endregion Constructors
 
+        #region Methods
+        /// <summary>
+        /// Adds a single Production to the set
+        /// </summary>
+        /// <param name="p">Production to add</param>
         public void AddProduction(Production p)
         {
             _productionSet.Add(new List<Production>() { p });
         }
 
+        /// <summary>
+        /// Adds a stochastic Production set to the set
+        /// </summary>
+        /// <param name="lP">Stochastic Productions as a List of Production</param>
         public void AddStochasticProductionSet(List<Production> lP)
         {
             _productionSet.Add(lP);
         }
 
+        /// <summary>
+        /// Finds a applicable Production and applies it (if one is found)
+        /// </summary>
+        /// <param name="leftContext">Left Context Module</param>
+        /// <param name="predecessor">Predecessor Module</param>
+        /// <param name="rightContext">Right Context Module</param>
+        /// <param name="generation">Generation</param>
+        /// <returns>Successor as a List of Modules, or null if none apply</returns>
         public List<Module> ApplyTo(Module leftContext, Module predecessor, Module rightContext, int generation)
         {
             List<Module> successor = null;
@@ -42,12 +60,12 @@ namespace LSys
             {
                 if (applicableSet.Count > 1)
                 {
-                    successor = GetSuccessorFromStochastic(applicableSet, leftContext, predecessor,
+                    successor = GetSuccessorFromStochastic(applicableSet, leftContext, predecessor, 
                                                            rightContext, generation);
                 }
                 else
                 {
-                    successor = GetSuccessorFromNonStochastic(applicableSet, leftContext, predecessor,
+                    successor = GetSuccessorFromNonStochastic(applicableSet, leftContext, predecessor, 
                                                               rightContext, generation);
                 }
             }
@@ -58,14 +76,32 @@ namespace LSys
             return successor;
         }
 
-        private List<Module> GetSuccessorFromNonStochastic(List<Production> lP, Module leftContext,
+        /// <summary>
+        /// Determines the successor from a non-stochastic Production
+        /// </summary>
+        /// <param name="lP">Applicable Production</param>
+        /// <param name="leftContext">Left Context Module</param>
+        /// <param name="predecessor">Predecessor Module</param>
+        /// <param name="rightContext">Right Context Module</param>
+        /// <param name="generation">Generation</param>
+        /// <returns>Successor as List of Modules</returns>
+        private List<Module> GetSuccessorFromNonStochastic(List<Production> lP, Module leftContext, 
                                                            Module predecessor, Module rightContext,
                                                            int generation)
         {
             return lP[0].GetSuccessor(leftContext, predecessor, rightContext, generation);
         }
 
-        private List<Module> GetSuccessorFromStochastic(List<Production> lP, Module leftContext,
+        /// <summary>
+        /// Determines the successor from a stochastic Production set
+        /// </summary>
+        /// <param name="lP">Applicable Production Set</param>
+        /// <param name="leftContext">Left Context Module</param>
+        /// <param name="predecessor">Predecessor Module</param>
+        /// <param name="rightContext">Right Context Module</param>
+        /// <param name="generation">Generation</param>
+        /// <returns>Successor as List of Modules</returns>
+        private List<Module> GetSuccessorFromStochastic(List<Production> lP, Module leftContext, 
                                                         Module predecessor, Module rightContext,
                                                         int generation)
         {
@@ -86,6 +122,6 @@ namespace LSys
             }
             return successor;
         }
-
+        #endregion Methods
     }
 }
