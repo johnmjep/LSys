a7398e47ae94405c04dbe4699fbb5df63e5ea285
John Jepson <JohnMJep@googlemail.com>
2016-08-09 11:59:31 +0100

    Update Production.cs
    - Removed unnecessary references
    - Added comments
    - Improved robustness of AppliesTo

--- LSys/Production.cs
+++ LSys/Production.cs
@@ -1,14 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LSys
 {
+    /// <summary>
+    /// Class to define an L-System Production
+    /// </summary>
     public class Production
     {
-        // id : lc < pred > rc : cond -> succ : prob
+        // Form: id : lc < pred > rc : cond -> succ : prob
+
+        #region Fields
         public int ID { get; private set; }
         public Module LeftContext { get; private set; }
         public Module StrictPredecessor { get; private set; }
@@ -29,7 +31,14 @@ namespace LSys
             }
         }
         public double Probability;
+        #endregion Fields
 
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="strictPredecessor">Strict Predecessor  Module of Production</param>
+        /// <param name="successor">Production Successor as List of Modules</param>
         public Production(Module strictPredecessor, List<Module> successor)
         {
             StrictPredecessor = strictPredecessor;
@@ -40,7 +49,11 @@ namespace LSys
             Condition = null;
             Probability = 1.0;
         }
-
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="p">Production to clone</param>
         public Production(Production p)
         {
             ID = p.ID;
@@ -52,87 +65,148 @@ namespace LSys
             _successor = p.Successor;
             Probability = p.Probability;
         }
+        #endregion Constructors
 
+        #region Methods
+        /// <summary>
+        /// Returns the successor of this Production
+        /// </summary>
+        /// <param name="leftContext">Left Context of the Predecessor</param>
+        /// <param name="predecessor">Predecessor</param>
+        /// <param name="rightContext">Right Context of the Predecessor</param>
+        /// <param name="generation">The Generation of the L-System</param>
+        /// <returns>Successor as List of Modules</returns>
         public List<Module> GetSuccessor(Module leftContext, Module predecessor, Module rightContext, int generation)
         {
-            List<Module> successor = Successor;
+            List<Module> successor = Successor;            
             ParameterAction?.Invoke(successor, leftContext, predecessor, rightContext, generation);
             return successor;
         }
 
+        /// <summary>
+        /// Determines if this production applies to a successor module
+        /// </summary>
+        /// <param name="leftContext">Left Context of the Predecessor</param>
+        /// <param name="predecessor">Predecessor</param>
+        /// <param name="rightContext">Right Context of the Predecessor</param>
+        /// <returns>True if this Production applies</returns>
         public bool AppliesTo(Module leftContext, Module predecessor, Module rightContext)
         {
             // TODO: Tidy up and improve robustness
             bool appliesTo = true;
-            if (LeftContext != null)
+            if (StrictPredecessor.Name == predecessor.Name && StrictPredecessor.Parameters.Length == predecessor.Parameters.Length)
             {
-                if (leftContext == null || LeftContext.Name != leftContext.Name)
+                if (LeftContext != null)
+                {
+                    if (leftContext == null || LeftContext.Name != leftContext.Name)
+                    {
+                        appliesTo = false;
+                    }
+                }
+                if (RightContext != null)
+                {
+                    if (rightContext == null || RightContext.Name != rightContext.Name)
+                    {
+                        appliesTo = false;
+                    }
+                }
+                if (appliesTo != false && Condition != null && !Condition(leftContext, predecessor, rightContext))
                 {
                     appliesTo = false;
-                }
+                } 
             }
-            if (StrictPredecessor.Name != predecessor.Name)
-            {
-                appliesTo = false;
-            }
-            if (RightContext != null)
-            {
-                if (rightContext == null || RightContext.Name != rightContext.Name)
-                {
-                    appliesTo = false;
-                }
-            }
-            if (Condition != null && !Condition(leftContext, predecessor, rightContext))
+            else
             {
                 appliesTo = false;
             }
             return appliesTo;
         }
 
+        /// <summary>
+        /// Builder method for constructing a Production using method chaining
+        /// </summary>
+        /// <param name="strictPredecessor"></param>
+        /// <param name="successor"></param>
+        /// <returns></returns>
         public static Production Build(Module strictPredecessor, List<Module> successor)
         {
             return new Production(strictPredecessor, successor);
         }
 
+        /// <summary>
+        /// Sets the left context
+        /// </summary>
+        /// <param name="leftContext">Left Context Module</param>
+        /// <returns>This production for method chaining</returns>
         public Production SetLeftContext(Module leftContext)
         {
             LeftContext = leftContext;
             return this;
         }
 
+        /// <summary>
+        /// Sets the Right Context
+        /// </summary>
+        /// <param name="rightContext">Right Context Module</param>
+        /// <returns>This production for method chaining</returns>
         public Production SetRightContext(Module rightContext)
         {
             RightContext = rightContext;
             return this;
         }
 
+        /// <summary>
+        /// Sets the Condition Function
+        /// </summary>
+        /// <param name="condition">Condition Func</param>
+        /// <returns>This production for method chaining</returns>
         public Production SetCondition(Func<Module, Module, Module, bool> condition)
         {
             Condition = condition;
             return this;
         }
 
+        /// <summary>
+        /// Sets the Action that determines successor parameters
+        /// </summary>
+        /// <param name="pAction">Successor Parameter Action</param>
+        /// <returns>This production for method chaining</returns>
         public Production SetParameterAction(Action<List<Module>, Module, Module, Module, int> pAction)
         {
             ParameterAction = pAction;
             return this;
         }
 
+        /// <summary>
+        /// Sets the probability (used for stochastic productions)
+        /// </summary>
+        /// <param name="probability">Stochastic Production Probability</param>
+        /// <returns>This production for method chaining</returns>
         public Production SetProbability(double probability)
         {
             Probability = probability;
             return this;
         }
 
+        /// <summary>
+        /// Sets the Successor 
+        /// </summary>
+        /// <param name="successor">Successor as List of Modules</param>
+        /// <returns>This production for method chaining</returns>
         public Production SetSuccessor(List<Module> successor)
         {
             _successor = successor;
             return this;
         }
 
+        /// <summary>
+        /// Returns a copy of this Production (useful for creating stochastic production sets)
+        /// </summary>
+        /// <returns>A copy of this Production</returns>
         public Production Copy()
         {
             return new Production(this);
         }
+        #endregion Methods
     }
 }
